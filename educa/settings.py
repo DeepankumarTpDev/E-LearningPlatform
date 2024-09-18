e03ef6f99448f5d9700d2357026331fd83c90a35
Deepankumar <deepankumar@testpress.in>
2024-09-18 04:35:11 +0000

    Add Authentication Templates

--- educa/settings.py
+++ educa/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Ensure this points to your static directory
 
 # Application definition
 
@@ -117,5 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# settings.py
 
-STATIC_URL = '/static/'
+
