02acd8b422cbc2912632bf7dcdaf15cd6344c314
Deepankumar <deepankumar@testpress.in>
2024-09-18 08:14:34 +0000

    Add generic Class based view to Add Contents to a Module

--- courses/models.py
+++ courses/models.py
@@ -58,8 +58,8 @@ class Content(models.Model):
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,
-    related_name='%(class)s_related',
-    on_delete=models.CASCADE)
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
