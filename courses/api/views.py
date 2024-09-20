416c6e3c6368eccc76134b12031f4955d71d0892
Deepankumar <deepankumar@testpress.in>
2024-09-20 11:36:00 +0000

    Handle Authentication,Add Permissions to views,Create Viewset,routers and Add additional Actions to viewsets

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from .serializers import SubjectSerializer
 from ..models import Subject, Course
-from rest_frameworks.views import APIView
-from rest_frameworks.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 class SubjectListView(generics.ListAPIView):
@@ -13,8 +13,29 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-class CourseEnrollView(APIView):
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+from rest_framework.authentication import BasicAuthentication
+from  rest_framework.permissions import IsAuthenticated
+# class CourseEnrollView(APIView):
+#     authentication_classes = (BasicAuthentication,)
+#     permission_classes = (IsAuthenticated,)
+
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
+    
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+from rest_framework.decorators import action
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
