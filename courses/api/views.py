862cc3db7ec7cfe48332185f5bec1aa31574171d
Deepankumar <deepankumar@testpress.in>
2024-09-20 07:21:52 +0000

    Build Custom APIView for Course Enroll

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .serializers import SubjectSerializer
-from ..models import Subject
+from ..models import Subject, Course
+from rest_frameworks.views import APIView
+from rest_frameworks.response import Response
+from django.shortcuts import get_object_or_404
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -9,3 +12,9 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
