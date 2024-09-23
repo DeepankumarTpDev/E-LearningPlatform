baf41e5d61036a6d310a99f34649d0a0ae9fbfdc
Deepankumar <deepankumar@testpress.in>
2024-09-23 11:43:39 +0000

    Add Context to the messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,13 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         # accept connection
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         # join room group
@@ -13,16 +16,19 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
         self.accept()
+
     def disconnect(self, close_code):
         # leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
-        # receive message from WebSocket
+
+    # receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
         # # send message to WebSocket
         # self.send(text_data=json.dumps({'message': message}))
         # send message to room group
@@ -31,6 +37,9 @@ class ChatConsumer(WebsocketConsumer):
             {
                 'type': 'chat_message',
                 'message': message,
+                'user': self.user.username, 
+                'datetime': now.isoformat(),
+
             }
         )
 
