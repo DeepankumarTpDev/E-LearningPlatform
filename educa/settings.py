4dc273f94df7bb38295a5b37156f726d8aa1cba5
Deepankumar <deepankumar@testpress.in>
2024-09-23 11:26:22 +0000

    Enable and setup Channel Layer with redis and create groups to chat room

--- educa/settings.py
+++ educa/settings.py
@@ -76,6 +76,15 @@ TEMPLATES = [
     },
 ]
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 WSGI_APPLICATION = 'educa.wsgi.application'
 ASGI_APPLICATION = 'educa.routing.application'
 
